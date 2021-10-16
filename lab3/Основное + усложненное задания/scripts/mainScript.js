a918937fcdbf126ddf60c0c698eb60c7d1a8bfc9
Eliseev Vadim <eliseevv02@mail.ru>
2021-10-16 16:53:31 +0300

    refactor: remove unused variables

--- lab3/Основное + усложненное задания/scripts/mainScript.js
+++ lab3/Основное + усложненное задания/scripts/mainScript.js
@@ -1,11 +1,7 @@
 let skyButton = document.getElementById('sky-button');
-let stars = document.getElementById('starsArea');
-let cloud = document.getElementById('cloud');
-let moonDots = document.getElementById('moonDots');
-let sun = document.getElementById('sun');
 
-let flag = false;
 function rollBack() {
     skyButton.setAttribute('rel', 'rollBack');
 }
+
 skyButton.addEventListener("click", rollBack);
