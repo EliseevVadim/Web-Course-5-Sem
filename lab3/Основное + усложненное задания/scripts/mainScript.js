4f5e64cf3bcde03f8f0ebf83b0f3047813772530
Eliseev Vadim <eliseevv02@mail.ru>
2021-10-16 16:51:45 +0300

    refactor: give more animation responsibility from JS to CSS

--- lab3/Основное + усложненное задания/scripts/mainScript.js
+++ lab3/Основное + усложненное задания/scripts/mainScript.js
@@ -4,25 +4,8 @@ let cloud = document.getElementById('cloud');
 let moonDots = document.getElementById('moonDots');
 let sun = document.getElementById('sun');
 
+let flag = false;
 function rollBack() {
-    skyButton.style.animationName = "buttonUpdate";
-    skyButton.style.animationDuration = "3s";
-    skyButton.style.animationDirection = "reverse";
-    stars.style.animationDelay = "3s";
-    stars.style.animationDuration = "2s";
-    stars.style.animationName = "showStars";
-    stars.style.animationDirection = "reverse";
-    sun.style.animationDelay = "5s, 7s";
-    sun.style.animationDuration = "2s, 5s";
-    sun.style.animationName = "repaintSun, moveSun";
-    sun.style.animationDirection = "reverse";
-    moonDots.style.animationDelay = "5s";
-    moonDots.style.animationDuration = "2s";
-    moonDots.style.animationName = "showDots";
-    moonDots.style.animationDirection = "reverse";
-    cloud.style.animationDelay = "5s, 7s";
-    cloud.style.animationDuration = "2s, 5s";
-    cloud.style.animationName = "hideCloud, moveCloud";
-    cloud.style.animationDirection = "reverse";
+    skyButton.setAttribute('rel', 'rollBack');
 }
 skyButton.addEventListener("click", rollBack);
