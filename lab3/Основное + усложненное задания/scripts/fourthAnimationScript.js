953ac32380b551fff277a2d3d3e06ea641db1c08
Eliseev Vadim <eliseevv02@mail.ru>
2021-10-17 10:50:16 +0300

    feat: finish fourth animation

--- lab3/Основное + усложненное задания/scripts/fourthAnimationScript.js
+++ lab3/Основное + усложненное задания/scripts/fourthAnimationScript.js
@@ -1,13 +1,45 @@
 let writeableSpan = document.getElementById('field');
 let leftArrow = document.getElementById('leftArrow');
 let  rightArrow = document.getElementById('rightArrow');
-let arrowsTops = document.getElementsByClassName('arrow-top');
-let arrowsBottoms = document.getElementsByClassName('arrow-bottom');
 
-if (writeableSpan.textContent === '1'){
-    leftArrow.style.opacity = '0.5';
-    arrowsTops[0].style.transform = 'rotate(0deg)';
-    arrowsBottoms[0].style.transform = 'rotate(0deg)';
-    leftArrow.setAttribute('aria-disabled', 'true');
+function leftArrowClick() {
+
+    if (leftArrow.getAttribute('rel')==='common') {
+        leftArrow.setAttribute('rel', '');
+        let value = parseInt(writeableSpan.textContent);
+        value--;
+        writeableSpan.textContent = String(value);
+        if (writeableSpan.textContent === '1'){
+            leftArrow.setAttribute('rel', 'disabled');
+        }
+        if (writeableSpan.textContent === '4'){
+            rightArrow.setAttribute('rel', 'enabled');
+            setTimeout(()=>rightArrow.setAttribute('rel', 'common'), 1000);
+        }
+        if (leftArrow.getAttribute('rel')!=='disabled') {
+            setTimeout(()=>leftArrow.setAttribute('rel', 'common'), 100);
+        }
+    }
 }
 
+function rightArrowClick() {
+    if (rightArrow.getAttribute('rel')==='common') {
+        rightArrow.setAttribute('rel', '');
+        let value = parseInt(writeableSpan.textContent);
+        value++;
+        writeableSpan.textContent = String(value);
+        if (writeableSpan.textContent === '5'){
+            rightArrow.setAttribute('rel', 'disabled');
+        }
+        if (writeableSpan.textContent === '2'){
+            leftArrow.setAttribute('rel', 'enabled');
+            setTimeout(()=>leftArrow.setAttribute('rel', 'common'), 1000);
+        }
+        if (rightArrow.getAttribute('rel')!=='disabled') {
+            setTimeout(()=>rightArrow.setAttribute('rel', 'common'), 100);
+        }
+    }
+}
+
+leftArrow.addEventListener('click', leftArrowClick);
+rightArrow.addEventListener('click', rightArrowClick);
