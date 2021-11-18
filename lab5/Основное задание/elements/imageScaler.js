f486e13447d2abea747961ca79f9f45e718da7e9
Eliseev Vadim <eliseevv02@mail.ru>
2021-11-18 12:35:02 +0300

    chore: remove unnecessary checks and output

--- lab5/Основное задание/elements/imageScaler.js
+++ lab5/Основное задание/elements/imageScaler.js
@@ -6,23 +6,18 @@ let image = currentContainer.children[0];
 let zoomPoint = currentContainer.children[1];
 let url = image.getAttribute('src');
 zoomPoint.style.backgroundImage = 'url(' + url +')';
-console.log(zoomPoint.style.cssText);
 let width = image.width;
 let height = image.height;
 zoomPoint.style.backgroundSize = `${width * scaleCoefficient}px ${height * scaleCoefficient}px`;
 
 currentContainer.onmousemove = function (e) {
     zoomPoint.style.display = 'block';
-    console.log(e.offsetY, e.offsetY);
     let x = e.offsetX;
     let y = e.offsetY;
-    if (x > 0 && y > 0) {
-        console.log(x, y);
-        zoomPoint.style.left = `${x}px`;
-        zoomPoint.style.top = `${y}px`;
-        zoomPoint.style.backgroundPositionX = `${-x * scaleCoefficient}px`;
-        zoomPoint.style.backgroundPositionY = `${-y * scaleCoefficient}px`;
-    }
+    zoomPoint.style.left = `${x}px`;
+    zoomPoint.style.top = `${y}px`;
+    zoomPoint.style.backgroundPositionX = `${-x * scaleCoefficient}px`;
+    zoomPoint.style.backgroundPositionY = `${-y * scaleCoefficient}px`;
 }
 
 currentContainer.onmouseout = function () {
